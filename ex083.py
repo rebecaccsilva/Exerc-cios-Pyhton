a8105e53007a089538c16319502e20269ab124e0
Rebeca Silva <rebecaccsilvadev@gmail.com>
2026-04-01 21:51:41 -0300

    ex083

---
+++ ex083.py
@@ -0,0 +1,19 @@
+#Crie um programa onde o usuário digite uma expressão qualquer que use parênteses. Seu aplicativo deverá analisar se a expressão passada está com os parênteses abertos e fechados na ordem correta.
+
+exp=str(input('Digite a expressão: ')) 
+pilha=[]
+
+for sim in exp:
+    if sim == '(':
+        pilha.append('(')
+    elif sim == ')':
+        if len(pilha)>0:
+            pilha.pop()
+        else:
+            pilha.append(')')
+            break
+
+if len(pilha)==0:
+    print('Sua expressão está válida!')
+else:
+    print('Sua expressão está errada!!!')
