f218bf1c9f686c5c05c930f12233e49ccfd1d8ba
Rebeca Silva <rebecaccsilvadev@gmail.com>
2026-04-17 16:23:15 -0300

    .

---
+++ ex097.py
@@ -0,0 +1,12 @@
+# Faça um programa que tenha uma função chamada escreva(), que receba um texto qualquer como parâmetro e mostre uma mensagem com tamanho adaptável.
+
+def escreva(txt):
+    print('~'*(len(txt)+4))
+    print(f'  {txt}  ')
+    print('~'*(len(txt)+4))
+
+
+escreva('Rebeca Cristina')
+escreva('Curso em Vídeo Python')
+escreva('Esqueça tudo')
+escreva('RECUPERAMO A BOLAAAAAAA')
