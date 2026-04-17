cc163c86a7d48a865ac47cb7cc9ed4ba55cbf72c
Rebeca Silva <rebecaccsilvadev@gmail.com>
2026-04-17 16:23:29 -0300

    .

---
+++ ex099.py
@@ -0,0 +1,25 @@
+# Faça um programa que tenha uma função chamada maior(), que receba vários parâmetros com valores inteiros. Seu programa tem que analisar todos os valores e dizer qual deles é o maior.
+
+from time import sleep
+
+def maior(*num):
+    cont= maior=0
+    print('-='*25)
+    print('Analisando números passados....')
+    for valor in num:
+        print(f'{valor} ', end='', flush=True)
+        sleep(0.5)
+        if cont==0:
+            maior=valor
+        else:
+            if valor>maior:
+                maior=valor
+        cont+=1
+    print(f'Foram informados {cont} valores ao todo')
+    print(f'O maior valor informado foi {maior}')
+    sleep(2.5)
+
+
+maior(2,3,9,1,0,7,4)
+maior(1,7)
+maior(6,5,4)
