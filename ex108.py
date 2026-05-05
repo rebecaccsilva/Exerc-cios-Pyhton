e05baf507142ad0a23b5da28207013c08c7eb894
Rebeca Silva <rebecaccsilvadev@gmail.com>
2026-05-05 20:50:10 -0300

    .

---
+++ ex108.py
@@ -0,0 +1,8 @@
+# Adapte o código do desafio 107, criando uma função adicional chamada moeda() que consiga mostrar os números como um valor monetário formatado.
+import ex107a
+
+valor = float(input("Digite o valor: R$"))
+
+print(f"Aumentando 20%, temos R${ex107a.moeda(ex107a.aumento(valor,20))}")
+print(f"O dobro de R${valor} é R${ex107a.moeda(ex107a.dobro(valor))}")
+print(f"A metade de R${valor} é R${ex107a.moeda(ex107a.metade(valor))}")
