9bf6e4cd4ed42cec9c1a0603c24c777e900a7143
Rebeca Silva <rebecaccsilvadev@gmail.com>
2026-05-06 15:49:42 -0300

    .

--- ex107a.py
+++ ex107a.py
@@ -1,8 +1,8 @@
 # Crie um módulo chamado moeda.py que tenha as funções incorporadas aumentar(), diminuir(), dobro() e metade(). Faça também um programa que importe esse módulo e use algumas dessas funções.
 
 
-def aumento(n=0, taxa=0, format=False):
-    au = (taxa / 100 * n) + n
+def aumento(n=0, taxaa=0, format=False):
+    au = (taxaa / 100 * n) + n
     # print(f'Aumentando {taxa}%, temos R${au:.2f}')
     return au if format is False else moeda(au)
 
@@ -19,10 +19,23 @@ def metade(n=0, format=False):
     return me if format is False else moeda(me)
 
 
-def desconto(n=0, taxa=0, format=False):
-    des = n - (taxa / 100 * n)
+def desconto(n=0, taxad=0, format=False):
+    des = n - (taxad / 100 * n)
     return des if format is False else moeda(des)
 
 
-def moeda(preço=0, moeda="R$"):
-    return f"{moeda}{preço:>.2f}".replace(".", ",")
+def moeda(n=0, moeda="R$"):
+    return f"{moeda}{n:>.2f}".replace(".", ",")
+
+
+def resumo(n=0, taxaa=0, taxad=0):
+    print('='*50)
+    print(f'RESUMO DO VALOR '.center(50))
+    print('='*50)
+    print(f'Preço analisado: \t{moeda(n)}')
+    print(f'{taxaa}% de aumento: \t{aumento(n,taxaa,True):.>6}')
+    print(f'{taxad}% de desconto: \t{desconto(n,taxad,True)}')
+    print(f'Dobro do preço: \t{dobro(n,True)}')
+    print(f'Metade do preço: \t{metade(n,True)}')
+    print('-'*50)
+
---
+++ ex110.py
@@ -0,0 +1,6 @@
+#Adicione o módulo moeda.py criado nos desafios anteriores, uma função chamada resumo(), que mostre na tela algumas informações geradas pelas funções que já temos no módulo criado até aqui
+import ex107a
+
+valor= float(input('Digite o preço: R$'))
+
+ex107a.resumo(valor, 20,12)
