317cb9abf90026017b8790437cb7d8415e88123b
Rebeca Silva <rebecaccsilvadev@gmail.com>
2026-05-26 16:37:12 -0300

    .

--- PythonPOO/003/ex003.py
+++ PythonPOO/003/ex003.py
@@ -7,18 +7,26 @@ class ContaBancaria:
         self.id = id
         self.nome = nome
         self.saldo = saldo
-        print(f"\033[1;33mConta {self.id} criada com sucesso. Saldo atual de R${self.saldo:,.2f}\033[m")
+        print(
+            f"\033[1;33mConta {self.id} criada com sucesso. Saldo atual de R${self.saldo:,.2f}\033[m"
+        )
 
     def depositar(self, valor):
         self.saldo += valor
-        print(f"\033[1;32mDepósito de R${valor:,.2f} autorizado na conta {self.id}.\033[m")
+        print(
+            f"\033[1;32mDepósito de R${valor:,.2f} autorizado na conta {self.id}.\033[m"
+        )
 
     def sacar(self, valor):
-        if valor> self.saldo:
-            print(f'\033[1;31mSAQUE NEGADO de R${valor:,.2f} na conta {self.id}: SALDO INSUFICIENTE!!\033[m')
+        if valor > self.saldo:
+            print(
+                f"\033[1;31mSAQUE NEGADO de R${valor:,.2f} na conta {self.id}: SALDO INSUFICIENTE!!\033[m"
+            )
         else:
             self.saldo -= valor
-            print(f"\033[1;32mSaque de R${valor:,.2f} autorizado na conta {self.id}.\033[m ")
+            print(
+                f"\033[1;32mSaque de R${valor:,.2f} autorizado na conta {self.id}.\033[m "
+            )
 
     def __str__(self):
         return f"\033[1;35mA conta {self.id} de {self.nome} tem R${self.saldo:,.2f} de saldo.\033[m"
