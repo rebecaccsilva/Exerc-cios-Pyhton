faa72da7f0c676e730fb0e193eeda863c590ac63
Rebeca Silva <rebecaccsilvadev@gmail.com>
2026-06-03 16:54:24 -0300

    .

--- PythonPOO/des20.py
+++ PythonPOO/des20.py
@@ -1,6 +1,5 @@
 from rich import print
 from rich.panel import Panel
-from rich import inspect
 
 
 class Gamer:
---
+++ PythonPOO/des21.py
@@ -0,0 +1,59 @@
+from rich import print
+
+
+class Caneta:
+    def __init__(self, cor):
+        self.tampada = True
+        # 1.
+        # if self.tinta.lower() == "azul":
+        # self.tinta = "blue"
+        # elif self.tinta.lower() == "vermelho":
+        # self.tinta = "red"
+        # elif self.tinta.lower() == "verde":
+        # self.tinta = "green"
+        # elif self.tinta.lower() == "roxo":
+        #   self.tinta = "purple"
+        # elif self.tinta.lower() == "amarelo":
+        #   self.tinta = "yellow"
+        # elif self.tinta.lower() == "preto":
+        #   self.tinta = "black"
+
+        # 2.
+        tradutor_cores = {
+            "azul": "[blue]",
+            "vermelho": "[red]",
+            "verde": "[green]",
+            "roxo": "[purple]",
+            "amarelo": "[yellow]",
+            "rosa":"[magenta]",
+            "branco":"[white]"
+        }
+        cor_comum = cor.lower()
+        self.tinta = tradutor_cores.get(cor_comum, "unknown")
+
+    def destampar(self):
+        self.tampada = False
+
+    def tampar(self):
+        if self.tampada== True:
+            print(f'A {self.tinta}caneta[/] já está tampada!!')
+        else: 
+            self.tampada = True
+
+    def quebrar_linha(self, li):
+        print("\n" * li, end="")
+
+    def escrever(self, msg):
+        if self.tampada:
+            print(f":prohibited: A {self.tinta}caneta[/] está tampada!")
+        else:
+            print(f"{self.tinta}{msg}[/]", end="  ")
+
+
+c1 = Caneta("amarelo")
+c1.tampar()
+c1.escrever("Hellow World")
+c2 = Caneta("rosa")
+c2.destampar()
+c1.quebrar_linha(3)
+c2.escrever("Blebleblubla")
